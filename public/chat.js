a2e7f58270b2901653bbbaaa8bf3e94a49410cca
Chris Best <chr2sbest@gmail.com>
2014-06-21 19:32:29 -0700

    chat server with username up and running!

--- public/chat.js
+++ public/chat.js
@@ -3,21 +3,27 @@ window.onload = function() {
     ,   socket = io.connect('http://localhost:5000') 
     ,   field = document.getElementById('field') 
     ,   sendButton = document.getElementById('send')
-    ,   content = document.getElementById('content');
+    ,   content = document.getElementById('content')
+    ,   name = document.getElementById('name');
 
     //Send message on 'send' click event
     sendButton.onclick = function() {
         var text = field.value;
-        socket.emit('send', {message: text});
+        var username = name.value;
+        console.log(username);
+        if (!username) {alert("Enter name!"); return;}
+        socket.emit('send', {message: text, username: username});
     };
     
     //Message handling
     socket.on('message', function (data) {
         if (data.message) {
             var messageHTML = ''
-            messages.push(data.message)
+            messages.push(data)
             for(var i = 0; i < messages.length; i++) {
-                messageHTML += messages[i] + '<br>'
+                var newmsg = '<b>' + (messages[i].username ? messages[i].username : 'Server') + 
+                    ': </b>' + messages[i].message + '<br />';
+                messageHTML += newmsg
             };
             content.innerHTML = messageHTML;
         } else {
